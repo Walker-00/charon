ef3ac11364fb190122ae8e6a10fe729780e0f530
Linus Walker <rissk_it@proton.me>
2025-02-11 18:16:53 +0630

    chore: clippy arg struct in general

---
+++ src/structures/general.rs
@@ -0,0 +1,23 @@
+#[derive(clap::Parser, Debug)]
+#[command(
+    version,
+    about = "Charon: The Proxy Server",
+    long_about = "Charon is a proxy server, built on Pingora, that ferries packets across the digital river—transferring data from the chaotic internet to servers, much like the mythical Charon guided souls to the underworld."
+)]
+pub struct Args {
+    /// Configuration file path
+    #[arg(short, long)]
+    pub config: Option<String>,
+
+    /// Get Example Full Config
+    #[arg(short = 'e', long)]
+    pub example: bool,
+
+    /// Get Example Proxy Config
+    #[arg(short = 'p', long)]
+    pub example_proxy: bool,
+
+    /// Get Example Load Balancer Config
+    #[arg(short = 'l', long)]
+    pub example_load_balancer: bool,
+}
